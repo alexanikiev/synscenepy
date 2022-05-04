292e22f1e1955b2c93c27728c06e1c79b18ef9a3
Alex Anikiev <ani.kiev@live.com>
2022-05-04 14:38:59 -0700

    Update scene.py

--- scene.py
+++ scene.py
@@ -25,36 +25,36 @@ class Scene:
     cast = []
     points = []
     while True:
-        # Signal = True, Noise = False
-        i = random.choice([True, False])
+      # Signal = True, Noise = False
+      i = random.choice([True, False])
 
-        if i:
-            signal += 1
-        else:
-            noise += 1
-        
-        if signal > self.signal and noise > self.noise:
-            break
-        
-        if i and signal > self.signal:
-            continue
-        if not i and noise > self.noise:
-            continue
-        
-        j = random.randint(0, len(self.characters)-1)
-        character = self.characters[j]
+      if i:
+        signal += 1
+      else:
+        noise += 1
 
-        x = 0
-        y = 0
-        while True:
-            x = random.randint(0, self.config.scene_width)
-            y = random.randint(0, self.config.scene_height - round(self.config.character_height/2))
-            if (self.config.calc_mask(i, x, y, points)):
-                break
+      if signal > self.signal and noise > self.noise:
+        break
 
-        layer = self.config.calc_mask_layer(x, y)
-        cast.append({'layer': layer, 'character_x': x, 'character_y': y, 'person': character})
-        points.append((x, y))
+      if i and signal > self.signal:
+        continue
+      if not i and noise > self.noise:
+        continue
+
+      j = random.randint(0, len(self.characters)-1)
+      character = self.characters[j]
+
+      x = 0
+      y = 0
+      while True:
+        x = random.randint(0, self.config.scene_width)
+        y = random.randint(0, self.config.scene_height - round(self.config.character_height/2))
+        if (self.config.calc_mask(i, x, y, points)):
+          break
+
+      layer = self.config.calc_mask_layer(x, y)
+      cast.append({'layer': layer, 'character_x': x, 'character_y': y, 'person': character})
+      points.append((x, y))
 
     cast = sorted(cast, key=lambda c: c['layer'])
     return cast
@@ -66,64 +66,64 @@ class Scene:
     self.cast = self.calc_scene_cast()
 
   def synthesize(self):
-      scene = Image.open(self.config.empty_scene_path)
-      for cast_character in self.cast:
-          layer = cast_character['layer']
-          x = cast_character['character_x']
-          y = cast_character['character_y']
-          character = cast_character['person']
+    scene = Image.open(self.config.empty_scene_path)
+    for cast_character in self.cast:
+      layer = cast_character['layer']
+      x = cast_character['character_x']
+      y = cast_character['character_y']
+      character = cast_character['person']
 
-          model = Image.open("./models/" + character.name + "/" + character.calc_character_image())
+      model = Image.open("./models/" + character.name + "/" + character.calc_character_image())
 
-          scale_pct = self.config.calc_scale_pct(layer)
-          model_width, model_height = model.size
-          model_height_pct = (character.height/float(model_height))
-          model_width = int((float(model_width)*float(model_height_pct)))
-          model_size = round(model_width*scale_pct), round(character.height*scale_pct)
-          model = model.resize(model_size)
+      scale_pct = self.config.calc_scale_pct(layer)
+      model_width, model_height = model.size
+      model_height_pct = (character.height/float(model_height))
+      model_width = int((float(model_width)*float(model_height_pct)))
+      model_size = round(model_width*scale_pct), round(character.height*scale_pct)
+      model = model.resize(model_size)
 
-          model = self.synthesize_model_position(model)
+      model = self.synthesize_model_position(model)
 
-          model = self.synthesize_model_color(model)
-          model = self.synthesize_model_contrast(model)
-          model = self.synthesize_model_sharpness(model)
-          model = self.synthesize_model_brightness(model)
+      model = self.synthesize_model_color(model)
+      model = self.synthesize_model_contrast(model)
+      model = self.synthesize_model_sharpness(model)
+      model = self.synthesize_model_brightness(model)
 
-          model = self.synthesize_model_pixelate(model)
+      model = self.synthesize_model_pixelate(model)
 
-          scene.paste(model, (x, y), model)
+      scene.paste(model, (x, y), model)
 
-      scene.save(f"./labelled_images/{self.name}_{str(uuid.uuid4())}.png")
+    scene.save(f"./labelled_images/{self.name}_{str(uuid.uuid4())}.png")
 
   def synthesize_model_position(self, model):
-      i = random.choice([True, False])
-      if i:
-        model = model.transpose(Image.FLIP_LEFT_RIGHT)
-      return model
+    i = random.choice([True, False])
+    if i:
+      model = model.transpose(Image.FLIP_LEFT_RIGHT)
+    return model
 
   def synthesize_model_pixelate(self, model):
-      orig_size = model.size
-      factor = 1.2
-      model = model.resize(size=(round(orig_size[0] // factor), round(orig_size[1] // factor)), resample=0)
-      model = model.resize(orig_size, resample=0)
-      return model
+    orig_size = model.size
+    factor = 1.2
+    model = model.resize(size=(round(orig_size[0] // factor), round(orig_size[1] // factor)), resample=0)
+    model = model.resize(orig_size, resample=0)
+    return model
   
   def synthesize_model_color(self, model):
-      factor = 0.4
-      model = ImageEnhance.Color(model).enhance(factor)
-      return model
+    factor = 0.4
+    model = ImageEnhance.Color(model).enhance(factor)
+    return model
 
   def synthesize_model_contrast(self, model):
-      factor = 0.8
-      model = ImageEnhance.Contrast(model).enhance(factor)
-      return model
+    factor = 0.8
+    model = ImageEnhance.Contrast(model).enhance(factor)
+    return model
 
   def synthesize_model_sharpness(self, model):
-      factor = 0.2
-      model = ImageEnhance.Sharpness(model).enhance(factor)
-      return model
+    factor = 0.2
+    model = ImageEnhance.Sharpness(model).enhance(factor)
+    return model
 
   def synthesize_model_brightness(self, model):
-      factor = 0.6
-      model = ImageEnhance.Brightness(model).enhance(factor)
-      return model
+    factor = 0.6
+    model = ImageEnhance.Brightness(model).enhance(factor)
+    return model
